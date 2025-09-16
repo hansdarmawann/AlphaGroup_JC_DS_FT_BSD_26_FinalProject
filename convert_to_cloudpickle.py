d64159f2199c09c8c6e0b4e906cf75937a599e42
hansdarmawann <work.hansdarmawan001@gmail.com>
2025-09-16 12:25:43 +0700

    Update streamlit.

--- convert_to_cloudpickle.py
+++ convert_to_cloudpickle.py
@@ -2,7 +2,7 @@ import joblib
 import cloudpickle
 
 # Load your original joblib model
-model_bundle = joblib.load("Model/Model_Logreg_Telco_Churn_joblib.pkl")
+model_bundle = joblib.load("Model/Model_Logreg_7_fitur_Telco_Churn_joblib.pkl") 
 
 # Save as cloudpickle version
 with open("Model/Model_Logreg_Telco_Churn_cloud.pkl", "wb") as f:
